c7bd385ed35f803dc2b4cbcc21397f980657dd0c
Leonardo Lima <leonardoaugusto287@gmail.com>
2021-04-08 04:01:53 -0300

    feature: create and find on db

--- db/mysql.go
+++ db/mysql.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/leozz37/cartesian/models"
 )
 
 var MySQL *gorm.DB
@@ -18,4 +19,22 @@ func ConnectMySQL(databseType, dsn string) {
 
 func AutoMigration(model interface{}) {
 	MySQL.AutoMigrate(model)
+}
+
+func CreateCoordinate(coordinate models.Coordinate) error {
+	err := MySQL.FirstOrCreate(&coordinate).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindCoordinates() ([]models.Coordinate, error) {
+	var coordiantes []models.Coordinate
+
+	err := MySQL.Find(&coordiantes).Error
+	if err != nil {
+		return nil, err
+	}
+	return coordiantes, nil
 }

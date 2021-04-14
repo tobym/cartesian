c9314ab0969212b172af47ad946a10521d23b00d
Leonardo Lima <leonardoaugusto287@gmail.com>
2021-04-14 01:46:00 -0300

    feature: validating query values

--- handler/api.go
+++ handler/api.go
@@ -1,11 +1,47 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
 
-func FindDistance(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+func FindDistances(c *gin.Context) {
+	distance := c.Query("distance")
+	x := c.Query("x")
+	y := c.Query("y")
+
+	err := validateQuery(x, y, distance)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("%s | %s | %s", distance, x, y)})
+}
+
+func validateQueryValue(value string) error {
+	match, _ := regexp.MatchString("[+-]?([0-9]*[.])?[0-9]+", value)
+	if !match {
+		return errors.New(value + " is an invalid value")
+	}
+	return nil
+}
+
+func validateQuery(x, y, distance string) error {
+	if err := validateQueryValue(distance); err != nil {
+		return err
+	}
+	if err := validateQueryValue(x); err != nil {
+		return err
+	}
+	if err := validateQueryValue(y); err != nil {
+		return err
+	}
+	return nil
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/leozz37/cartesian/handler"
 )
@@ -8,7 +11,21 @@ import (
 func InitRoutes() {
 	r := gin.Default()
 
-	r.GET("/api/points", handler.FindDistance)
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	r.GET("/api/points", handler.FindDistances)
 	r.GET("points", handler.GetPoints)
 	r.NoRoute(noRoute)
 
